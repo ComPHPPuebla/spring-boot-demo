acc4707083807641e0e92f5cda39b8bca758c7e2
Luis Montealegre <montealegreluis@gmail.com>
2017-07-09 21:15:24 -0500

    chore: Fix validate.sh script
    * Travis runs maven install before validate.sh, therefore checking for
      the presence of the target folder is not reliable
    * Use git commands to check if incorrect files have been pushed

--- validate.sh
+++ validate.sh
@@ -2,17 +2,23 @@
 LINE=$'\n'
 ERROR=""
 
-if [ -e target ] && [ -d target ]
+COUNT=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master) | grep "^target" | wc -l)
+
+if [ "${COUNT}" -gt 0 ]
 then
     ERROR="${ERROR}* Do not commit your target directory${LINE}"
 fi
 
-if [ -e .idea ] && [ -d .idea ]
+COUNT=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master) | grep "^.idea" | wc -l)
+
+if [ "${COUNT}" -gt 0 ]
 then
     ERROR="${ERROR}* Do not commit your .idea directory${LINE}"
 fi
 
-if [ -e src/main/resources/application.properties ]
+COUNT=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master) | grep "^src/main/resources/application.properties" | wc -l)
+
+if [ "${COUNT}" -gt 0 ]
 then
     ERROR="${ERROR}* Do not commit your application.properties file${LINE}"
 fi
@@ -22,7 +28,7 @@ echo "${COUNT}"
 
 if [ "${COUNT}" -gt 20 ]
 then
-    ERROR="${ERROR}* Your commit includes too many files."
+    ERROR="${ERROR}* Your commit includes too many files: ${COUNT}${LINE}"
 fi
 
 if [ ! -z "$ERROR" ]
